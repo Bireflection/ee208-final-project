d2be64ac44799e5e75126b151d4e0e717b5f2d10
Bireflection <chen-ke@sjtu.edu.cn>
2022-11-10 16:05:38 +0800

    beautify

--- exercise2_crawler_photo.py
+++ exercise2_crawler_photo.py
@@ -8,7 +8,6 @@ import time
 import urllib.error
 import urllib.parse
 import urllib.request
-from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
 
@@ -92,8 +91,6 @@ def get_all_links(content, page):
         if full.match(i) == None:
             i = urllib.parse.urljoin(page, i)
         links.append(i)
-    # for i in range(1000):
-    #     links.append(page[:-2]+"{}".format(i))
     return links
 
 
